841f6f9707c0e6b86815f1727fd8ed3ea140be2b
victor <victor.fleury@gmail.com>
2015-01-05 11:17:54 +0100

    Listing projects done

--- hobbes/hobbes.py
+++ hobbes/hobbes.py
@@ -22,17 +22,40 @@ try:
 except:
     inMaya = False
 
-PATH_TO_PROJECT = '/home/victor/PROJECTS/'
+_PATH_TO_PROJECT = os.path.join('/home/victor/', 'PROJECTS/')
 
 class Hobbes(object):
     '''
         The Main class for loading assets, saving and versionning files
     '''
 
-    def __init__(self):
+    PROJECTS = _PATH_TO_PROJECT
+    
+    def __init__(self, verbose = False):
         '''
             Init of the class.
             Here we set the main variables we'll need to access
         '''
         
-        self._pathProject = PATH_TO_PROJECT
+        self._pathProject = PROJECTS
+        self.verbose = verbose
+    
+    
+    def _print(self, msg):
+        '''
+            A method to print output only if verbose is True
+        '''
+        if self.verbose:
+            print msg
+
+    def getListProjects(self):
+        '''
+            Getting allr the projects in the folder, to help build the UI and to load assets
+        '''
+        projects = []
+        
+        for project in os.listdir(self._pathProject):
+            fullPath = os.path.join(self._pathProject, project)
+            if os.path.isdir(fullPath):
+                projects.append(project)
+        return projects
